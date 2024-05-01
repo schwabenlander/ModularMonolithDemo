2afda7e4e2922090f7c438e29c031014a9e9c2ca
Sean Schwabenlander <sean@schwabenlander.com>
2024-04-30 22:00:27 -0500

    Added validator for CreateReviewRequest

---
+++ OnlineCourseReviews.Reviews/ReviewEndpoints/Create/CreateReviewRequestValidator.cs
@@ -0,0 +1,31 @@
+using FastEndpoints;
+using FluentValidation;
+
+namespace OnlineCourseReviews.Reviews.ReviewEndpoints.Create;
+
+internal class CreateReviewRequestValidator : Validator<CreateReviewRequest>
+{
+    public CreateReviewRequestValidator()
+    {
+        RuleFor(review => review.CourseId)
+            .NotEmpty()
+            .WithMessage("Course Id is required");
+
+        RuleFor(review => review.UserId)
+            .NotEmpty()
+            .WithMessage("User Id is required");
+
+        RuleFor(review => review.ReviewText)
+            .NotEmpty()
+            .WithMessage("Review text is required");
+
+        RuleFor(review => review.Rating)
+            .InclusiveBetween(1, 5)
+            .WithMessage("Rating must be a number between 1 and 5");
+
+        RuleFor(review => review.PricePaid)
+            .GreaterThanOrEqualTo(0)
+            .When(review => review.PricePaid.HasValue)
+            .WithMessage("Price paid must be greater than or equal to $0.00");
+    }
+}
